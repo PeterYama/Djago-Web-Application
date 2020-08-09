543077656726990c09679b521f791d511a6ae6a3
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-08-09 17:56:25 +1000

    added a custom index.html, and a custom admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
-# Register your models here.
+# Any changes to admin page must be registered here
+
+# Tells Django that Choice objects are edited on the Question admin page
+# That's pretty cool
+# Change the default admin.StackedInline to admin.TabularInline
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None,               {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date']}),
+    # ]
+    
+    # List the details of each item in questions in a table format
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # Add the inline method here
+    inlines = [ChoiceInline]
+    # Add a search box on admin's page
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
+
